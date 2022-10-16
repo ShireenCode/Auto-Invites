1e8a32b137b12588edf87c997c0024417f8d06bd
Shireen <66201059+ShireenCode@users.noreply.github.com>
2022-10-16 13:48:41 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+with open("./Input/Names/invited_names.txt", "r") as invitees:
+    invitee_list = invitees.readlines()
+
+with open("./Input/Letters/starting_letter.txt", "r") as starting_letter:
+    #starting_letter.replace("[name]", "Joe")
+    template = starting_letter.read()
+    print(template)
+
+for invitee in invitee_list:
+    new_invitee = invitee.strip()
+    new_invitee = new_invitee.strip("/n")
+    with open(f"./Output/ReadyToSend/{new_invitee}.txt", "w") as letter:
+        letter.write(str(template).replace("[name]", str(new_invitee)))
